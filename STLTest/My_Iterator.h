3274cae005b4b54e54edda214302ebb71935d956
wzp <798288416@qq.com>
2015-07-23 12:56:29 +0800

    增加了advance函数，使用std命名空间中提供的迭代器类型

--- STLTest/My_Iterator.h
+++ STLTest/My_Iterator.h
@@ -2,76 +2,141 @@
 #include "stdafx.h"
 
 
+
+
 namespace my_stl
 {
 
+	using std::input_iterator_tag;
+	using std::output_iterator_tag;
+	using std::forward_iterator_tag;
+	using std::random_access_iterator_tag;
+	using std::bidirectional_iterator_tag;
+	//五种迭代器类型 (五种迭代器类型之间存在继承关系)
 
-//五种迭代器类型 (五种迭代器类型之间存在继承关系)
+	//struct input_iterator_tag
+	//{
 
-struct input_iterator_tag
-{
+	//};
 
-};
+	//struct output_iterator_tag
+	//{
 
-struct output_iterator_tag
-{
+	//};
 
-};
+	//struct forward_iterator_tag : public input_iterator_tag
+	//{
 
-struct forward_iterator_tag: public input_iterator_tag
-{
+	//};
 
-};
+	//struct bidirection_iterator_tag : public forward_iterator_tag
+	//{
 
-struct bidirection_iterator_tag : public forward_iterator_tag
-{
+	//};
 
-};
+	//struct random_access_iterator_tag : public bidirection_iterator_tag
+	//{
 
-struct random_access_iterator_tag : public bidirection_iterator_tag
-{
+	//};
 
-};
-
-//template<typename Category,typename T, typename Distence=ptrdiff_t,typename Pointer=T*, typename Reference=T&>
-//struct iterator
-//{
-//	//迭代器的五种内置类型
-//	typedef Category iterator_category;
-//	typedef T value_type;
-//	typedef Distence difference_type;
-//	typedef Pointer pointer;
-//	typedef Reference reference;
-//};
+	template<typename Category, typename T, typename Distence = ptrdiff_t, typename Pointer = T*, typename Reference = T&>
+	struct iterator
+	{
+		//迭代器的五种内置类型
+		typedef Category iterator_category;
+		typedef T value_type;
+		typedef Distence difference_type;
+		typedef Pointer pointer;
+		typedef Reference reference;
+	};
 
 
-template<typename Iterator>
-struct iterator_traits
-{
-	//将迭代器的五种内置类型转换为统一的名称
-	typedef typename Iterator::iterator_category iterator_category;
-	typedef typename Iterator::value_type value_type;
-	typedef typename Iterator::difference_type difference_type;
-	typedef typename Iterator::pointer pointer;
-	typedef typename Iterator::reference reference;
-};
+	template<typename Iterator>
+	struct iterator_traits
+	{
+		//将迭代器的五种内置类型转换为统一的名称
+		typedef typename Iterator::iterator_category iterator_category;
+		typedef typename Iterator::value_type value_type;
+		typedef typename Iterator::difference_type difference_type;
+		typedef typename Iterator::pointer pointer;
+		typedef typename Iterator::reference reference;
+	};
 
-//此函数可以方便的取得迭代器的类型
-template<typename Iterator>
-inline typename iterator_traits<Iterator>::iterator_category iterator_catergory(const Iterator&)
-{
-	typedef typename iterator_traits<Iterator>::iterator_category category;
-	return catergory();
-	
-}
+	//此函数可以方便的取得迭代器的类型
+	template<typename Iterator>
+	inline typename iterator_traits<Iterator>::iterator_category iterator_category(const Iterator&)
+	{
+		typedef typename iterator_traits<Iterator>::iterator_category category;
+		return category();
 
-//此函数可以方便的取得迭代器的value_type
-template<typename Iterator>
-inline typename iterator_traits<Iterator>::value_type * value_type(const Iterator&)
-{
-	//返回一个指定类型的空指针
+	}
 
-	return static_cast<typename iterator_traits<Iterator>::value_type *>(0);
-}
+	//此函数可以方便的取得迭代器的value_type
+	template<typename Iterator>
+	inline typename iterator_traits<Iterator>::value_type * value_type(const Iterator&)
+	{
+		//返回一个指定类型的空指针
+		return static_cast<typename iterator_traits<Iterator>::value_type *>(0);
+	}
+
+	//针对原生指针设计的traits偏特化版本
+	template<typename T>
+	struct  iterator_traits<T*>
+	{
+		typedef random_access_iterator_tag iterator_category;
+		typedef T value_type;
+		typedef ptrdiff_t defference_type;
+		typedef T* pointer;
+		typedef T& reference;
+	};
+
+	//针对原声指针之pointer-to-const设计的片特化版本
+	template<typename T>
+	struct iterator_traits<const T*>
+	{
+		typedef random_access_iterator_tag iterator_category;
+		typedef T value_type;
+		typedef ptrdiff_t defference_type;
+		typedef const T* pointer;
+		typedef const T& reference;
+	};
+
+	//以下是整组advance函数
+	template<typename InputIterator, typename Distance>
+	inline void __advance(InputIterator &i, Distance n, input_iterator_tag)
+	{
+		while (--n)
+		{
+			++i;
+		}
+	}
+
+	template<typename InputIterator, typename Distance>
+	inline void __advance(InputIterator & i, typename Distance, bidirectional_iterator_tag)
+	{
+		if (n >= 0)
+		{
+			while (n--) ++i;
+		}
+		else
+		{
+			while (n++)
+			{
+				--i;
+			}
+		}
+	}
+
+	template<typename InputIterator, typename Distance>
+	inline void __advance(InputIterator & i, Distance n, random_access_iterator_tag)
+	{
+		i += n;
+	}
+
+	template<typename InputIterator, typename Distance>
+	inline void advance(InputIterator & i, Distance n)
+	{
+		__advance(i,n,iterator_category(i));
+	}
 
 }
--- STLTest/STLTest.cpp
+++ STLTest/STLTest.cpp
@@ -5,9 +5,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <list>
 #include "My_Iterator.h"
 
-using namespace std;
+using  std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::list;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -16,9 +21,38 @@ int _tmain(int argc, _TCHAR* argv[])
 	sv.push_back("second");
 	sv.push_back("third");
 	sv.push_back("fourth");
+	list<int> il;
+	il.push_back(1);
+	il.push_back(2);
+	il.push_back(3);
+
+	auto iter_il = il.begin();
+	
 	auto iter = sv.begin();
 	auto re= my_stl::value_type(iter);
 	cout << typeid(re).name() << endl;
+	auto type = my_stl::iterator_category(iter);
+	cout << "iter的类型为：";
+	cout << typeid(type).name() << endl;
+
+
+	my_stl::advance(iter, 2);
+	cout << *iter << endl;
+
+	/*int * p;
+	auto re1 = my_stl::value_type(p);
+	cout << typeid(re1).name() << endl;
+
+	cout << "输出迭代器的类型" << endl;
+	 auto re3 = my_stl::iterator_category(iter);
+	cout << typeid(re3).name() << endl;
+
+	auto re4 = my_stl::iterator_category(iter_il);
+	cout << typeid(re4).name() << endl;
+*/
+
 	return 0;
+	
+	
 }
 
